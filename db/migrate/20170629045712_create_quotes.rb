27c22074b00534f4b0ad00222b98cef9aa0ce746
AJ-DeGroot <alan.michael.degroot@gmail.com>
2018-04-12 23:09:02 -0400

    Codeship tweaks

--- db/migrate/20170629045712_create_quotes.rb
+++ db/migrate/20170629045712_create_quotes.rb
@@ -1,8 +1,8 @@
 class CreateQuotes < ActiveRecord::Migration[5.0]
   def change
     create_table :quotes do |t|
-      t.string :saying, :default => "Be happy"
-      t.string :author, :default => "Bob"
+      t.string :saying
+      t.string :author
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-Quote.create(saying: "Don't worry, be happy", author: 'One Dope Dude')
